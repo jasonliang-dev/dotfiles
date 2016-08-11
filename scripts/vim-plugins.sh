4950ebd632a8ff55d175ab15d1e31c6f459f71f6
SummerJacket <dxmcxd@gmail.com>
2016-08-11 15:01:11 -0700

    Update

--- scripts/vim-plugins.sh
+++ scripts/vim-plugins.sh
@@ -1,10 +1,6 @@
-mkdir -p $HOME/.vim/bundle
-cd $HOME/.vim/bundle
-if [ -d "$DIRECTORY" ]; then
-  git clone https://github.com/chriskempson/base16-vim
-  git clone https://github.com/raimondi/delimitmate
-  git clone https://github.com/tpope/vim-sensible
-  git clone https://github.com/tpope/vim-surround
-  git clone https://github.com/christoomey/vim-tmux-navigator
-  git clone https://github.com/valloric/youcompleteme
-fi
+git clone https://github.com/chriskempson/base16-vim
+git clone https://github.com/raimondi/delimitmate
+git clone https://github.com/tpope/vim-sensible
+git clone https://github.com/tpope/vim-surround
+git clone https://github.com/christoomey/vim-tmux-navigator
+git clone https://github.com/valloric/youcompleteme
