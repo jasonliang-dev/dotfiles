45e9a7ad17b110188104435ee2638ba7f104aacf
SummerJacket <dxmcxd@gmail.com>
2016-09-29 20:48:17 -0700

    Updated rofi.sh

--- scripts/rofi.sh
+++ scripts/rofi.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-rofi -show run -lines 1 -eh 1  -width 40  -padding 100 -opacity "97" -bw 0 -color-enabled -color-window "#2B303B" -color-normal "#2B303B","#E0E0E0","#2B303B","#2B303B","#2892a3" -font "Source Sans Pro 24"
+rofi -show run -lines 5 -eh 1 -width 100  -padding 345 -opacity 90 -bw 0 -color-enabled -color-window "#2B303B" -color-normal "#2B303B","#E0E0E0","#2B303B","#2B303B","#2892a3" -font "Source Sans Pro 24"
+
