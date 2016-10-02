a0b4599c34c866474ae1b65d29a0d1165374f325
SummerJacket <dxmcxd@gmail.com>
2016-10-01 20:41:02 -0700

    Blacker

--- scripts/rofi.sh
+++ scripts/rofi.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rofi -show run -lines 5 -eh 1 -width 100  -padding 345 -opacity 90 -bw 0 -color-enabled -color-window "#222222" -color-normal "#222222","#E0E0E0","#222222","#222222","#2892a3" -font "Source Sans Pro 24"
+rofi -show run -lines 5 -eh 1 -width 100  -padding 345 -opacity 90 -bw 0 -color-enabled -color-window "#151515" -color-normal "#151515","#E0E0E0","#151515","#151515","#2892a3" -font "Source Sans Pro 24"
 
