85c6363a3237415b69f907fcbcc1826988c7c2f0
SummerJacket <dxmcxd@gmail.com>
2016-10-01 19:31:17 -0700

    Black

--- scripts/rofi.sh
+++ scripts/rofi.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rofi -show run -lines 5 -eh 1 -width 100  -padding 345 -opacity 90 -bw 0 -color-enabled -color-window "#2B303B" -color-normal "#2B303B","#E0E0E0","#2B303B","#2B303B","#2892a3" -font "Source Sans Pro 24"
+rofi -show run -lines 5 -eh 1 -width 100  -padding 345 -opacity 90 -bw 0 -color-enabled -color-window "#222222" -color-normal "#222222","#E0E0E0","#222222","#222222","#2892a3" -font "Source Sans Pro 24"
 
