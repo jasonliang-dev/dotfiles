6417abbeb610c1969250a8eb2799dc2748dc81bb
SummerJacket <dxmcxd@gmail.com>
2016-10-30 23:26:18 -0700

    5 bars - 4 bars = 1 bar

--- scripts/bar.sh
+++ scripts/bar.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # I'm not very good at this...
+S() {
+    echo "            "
+}
 desktop() {
     ws=`xprop -root _NET_CURRENT_DESKTOP | awk '{print $3}'`
     case $ws in
@@ -18,7 +21,7 @@ desktop() {
 }
 window() {
     xprop -id $(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2) WM_CLASS | 
-    cut -d '"' -f2
+    cut -d '"' -f 2
 }
 barDate() {
     date "+%a, %B %d"
@@ -27,8 +30,8 @@ barTime() {
     date "+%I:%M %P"
 }
 power() {
-    percent="$(</sys/class/power_supply/BAT1/capacity)"
-    status="$(</sys/class/power_supply/BAT1/status)"
+    percent=$(</sys/class/power_supply/BAT1/capacity)
+    status=$(</sys/class/power_supply/BAT1/status)
 
     if [ $status == "Discharging" ]; then
         if [ $percent -lt 20 ]; then
@@ -58,40 +61,12 @@ volume() {
 
 font="-f sourcesanspro-9 -f fontawesome-10"
 
-# Center bar
-# Music
 while true; do
-    echo "%{c}$(music)"
+    echo "%{l}%{F#2B303B}%{B#C0C5CE}$(S)  $(desktop)$(S)%{F#EFF1F5}%{B#4F5B66}\
+              $(window)$(S)%{B#343D46}%{F#EFF1F5}\
+            %{c}$(music)\
+            %{r}%{F#EFF1F5}%{B#4F5B66}$(S)  $(barDate)$(S)  $(barTime)$(S)%{F#2B303B}%{B#C0C5CE}\
+            $(volume)$(S)$(power)$(S)%{B#343D46}"
     sleep 1
 done |
-lemonbar $font -g "598x28+384+0" -B "#343D46" -F "#EFF1F5" &
-
-# Left bar
-# Workspace
-while true; do
-    echo "%{c}  $(desktop)"
-    sleep 1
-done |
-lemonbar $font -g "128x28+0+0" -B "#C0C5CE" -F "#2B303B" &
-
-# Window title
-while true; do
-    echo "%{c}  $(window)"
-    sleep 1
-done |
-lemonbar $font -g "256x28+128+0" -B "#4F5B66" -F "#EFF1F5" &
-
-# Right bar
-# Volume and battery
-while true; do
-    echo "%{c}$(volume)        $(power)"
-    sleep 1
-done |
-lemonbar $font -g "128x28+1238+0" -B "#C0C5CE" -F "#2B303B" &
-
-# Date and time
-while true; do
-    echo "%{c}  $(barDate)          $(barTime)"
-    sleep 10
-done |
-lemonbar $font -g "256x28+982+0" -B "#4F5B66" -F "#EFF1F5" &
+lemonbar $font -g "1366x28+0+0" &
