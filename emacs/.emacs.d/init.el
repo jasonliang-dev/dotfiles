28fc09c29ada36cd48ad187681ee9a47d367f5ae
SummerJacket <dxmcxd@gmail.com>
2017-01-12 19:33:13 -0800

    Fixed Emacs config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -5,25 +5,3 @@
 (org-babel-load-file
  (expand-file-name "config.org"
 		   user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#2b303b" "#bf616a" "#a3be8c" "#ebcb8b" "#8fa1b3" "#b48ead" "#8fa1b3" "#c0c5ce"])
- '(ansi-term-color-vector
-   [unspecified "#2b303b" "#bf616a" "#a3be8c" "#ebcb8b" "#8fa1b3" "#b48ead" "#8fa1b3" "#c0c5ce"])
- '(custom-enabled-themes (quote (base16-ocean)))
- '(custom-safe-themes
-   (quote
-    ("78c1c89192e172436dbf892bd90562bc89e2cc3811b5f9506226e735a953a9c6" default)))
- '(package-selected-packages
-   (quote
-    (yasnippet rainbow-delimiters org-bullets neotree evil-org evil-leader linum-relative base16-theme magit evil-visual-mark-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
