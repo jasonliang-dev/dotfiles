21acf658214e7f623ffdcdf51e43ac96486dd161
SummerJacket <dxmcxd@gmail.com>
2017-01-30 14:55:12 -0800

    Rewrite Emacs config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -2,6 +2,4 @@
 (package-initialize)
 
 (require 'org)
-(org-babel-load-file
- (expand-file-name "config.org"
-		   user-emacs-directory))
+(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
