5e2c65edc3fad866922fd6d969de54b85100137a
SummerJacket <dxmcxd@gmail.com>
2017-02-11 14:25:52 -0800

    Update Emacs config. Git diffs in fringe

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -3,7 +3,7 @@
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-(set-fringe-mode 0)
+(set-fringe-mode 3)
 ;; yes/no prompts are y/n
 (fset 'yes-or-no-p 'y-or-n-p)
 
