be0cecb2a3d6cc316cbf472c027e38f89b5ba17c
SummerJacket <dxmcxd@gmail.com>
2017-08-13 20:50:00 -0700

    changes.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,21 +1,20 @@
+;; Custom
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(load custom-file)
+
 ;; remove bars
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (set-fringe-mode 3)
-;; open maximized
+
+;; open emacs maximized
 (toggle-frame-maximized)
+
 ;; yes/no prompts are y/n
 (fset 'yes-or-no-p 'y-or-n-p)
 
-
-(package-initialize)
-
-(setq custom-file (concat user-emacs-directory "custom.el"))
-(unless (file-exists-p custom-file)
-  (write-region "" nil custom-file))
-(load custom-file)
-
+;; Load package and add repos
 (require 'package)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
@@ -23,6 +22,7 @@
 (setq package-enable-at-startup nil)
 (package-initialize)
 
+;; Install use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
@@ -32,17 +32,33 @@
 
 (setq use-package-always-ensure t)
 
-(use-package base16-theme)
 
+;; ############
+;; Package list
+;; ############
+
+;; :init - before
+;; :config - after
+
+;; base16 colours
+(use-package base16-theme
+  :config
+  (load-theme 'base16-ocean t)
+  ;; define a variable used for other packages
+  (defvar lia/base16-colors base16-ocean-colors))
+
+;; text completion
 (use-package company
   :init
   (add-hook 'after-init-hook 'global-company-mode))
 
+;; hide or shorten minor modes
 (use-package diminish
   :disabled
   :init
   (diminish 'jiggle-mode))
 
+;; vim, in emacs
 (use-package evil
   :init
   ;; Scroll up with C-u
@@ -50,20 +66,28 @@
   :config
   ;; move by visual line
   (define-key evil-normal-state-map (kbd "j") 'evil-next-visual-line)
-  (define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line)
-  (use-package evil-leader
-    :init
-    (global-evil-leader-mode))
-  (use-package evil-numbers
-    :init
-    (global-set-key (kbd "C-c C-=") 'evil-numbers/inc-at-pt)
-    (global-set-key (kbd "C-c C--") 'evil-numbers/dec-at-pt))
-  (use-package evil-org))
+  (define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line))
 
+(use-package evil-leader
+  :ensure evil
+  :init
+  (global-evil-leader-mode))
+
+(use-package evil-numbers
+  :ensure evil
+  :init
+  (global-set-key (kbd "C-c C-=") 'evil-numbers/inc-at-pt)
+  (global-set-key (kbd "C-c C--") 'evil-numbers/dec-at-pt))
+
+(use-package evil-org
+  :ensure evil)
+
+;; syntax checking
 (use-package flycheck
   :init
   (add-hook 'after-init-hook #'global-flycheck-mode))
 
+;; fold marked regions
 (use-package folding
   :disabled
   :init
@@ -72,14 +96,15 @@
   :config
   (folding-add-to-marks-list 'web-mode "<!-- {{{ " "<!-- }}} -->" " -->" nil t))
 
+;; show git changes in gutter
 (use-package git-gutter-fringe
   :init
   (global-git-gutter-mode t)
   (setq fringes-outside-margins t)
   :config
-  (set-face-foreground 'git-gutter-fr:modified "#ebcb8b")
-  (set-face-foreground 'git-gutter-fr:added    "#a3be8c")
-  (set-face-foreground 'git-gutter-fr:deleted  "#bf616a")
+  (set-face-foreground 'git-gutter-fr:modified (plist-get lia/base16-colors :base0A))
+  (set-face-foreground 'git-gutter-fr:added    (plist-get lia/base16-colors :base0B))
+  (set-face-foreground 'git-gutter-fr:deleted  (plist-get lia/base16-colors :base08))
   ;; https://github.com/hlissner/.emacs.d/blob/master/core/core-vcs.el#L24
   (define-fringe-bitmap 'git-gutter-fr:added
     [224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224]
@@ -91,15 +116,20 @@
     [224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224]
     nil nil 'center))
 
+;; narrow lists
 (use-package helm
   :init
   (helm-mode 1)
   (global-set-key (kbd "M-x") 'helm-M-x))
 
-(use-package jdee)
+;; java add-on
+(use-package jdee
+  :disabled)
 
+;; relative line numbers
 (use-package linum-relative)
 
+;; git
 (use-package magit
   :config
   (defadvice magit-status (around magit-fullscreen activate)
@@ -115,46 +145,76 @@
 
   (define-key magit-status-mode-map (kbd "q") 'magit-quit-session))
 
+;; line numbers
 (use-package nlinum)
 
+;; cool looking bullets in org
 (use-package org-bullets
   :init
   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
   (setq org-bullets-bullet-list '("•")))
 
+;; powerline. it looks cool.
 (use-package powerline
   :disabled
   :config
   (use-package airline-themes))
 
+;; powerline with evil support
 (use-package powerline-evil
   :disabled
+  :ensure powerline
   :init
   (powerline-evil-vim-color-theme))
 
+;; rainbow brackets
 (use-package rainbow-delimiters
   :init
   (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
 
-(use-package web-mode)
+;; modeline from spacemacs
+(use-package spaceline
+  :config
+  (setq spaceline-highlight-face-func 'spaceline-highlight-face-evil-state)
+  ;; TODO: change the orange text to a nicer color
+  ;; (setq spaceline-face-func t)
+  (use-package spaceline-config
+    :ensure spaceline
+    :config
+    (spaceline-toggle-minor-modes-off)
+    (spaceline-spacemacs-theme)))
 
+;; neat features for web development
+(use-package web-mode
+  :disabled)
+
+;; templates
 (use-package yasnippet
   :disabled)
 
-(set-cursor-color "#c0c5ce")
+;; ##########
+;; Appearance
+;; ##########
 
+;; https://github.com/belak/base16-emacs#evil-mode
+;; Set the cursor color based on the evil state
+(setq evil-emacs-state-cursor   `(,(plist-get lia/base16-colors :base0E) box)
+      evil-insert-state-cursor  `(,(plist-get lia/base16-colors :base0D) bar)
+      evil-motion-state-cursor  `(,(plist-get lia/base16-colors :base0A) box)
+      evil-normal-state-cursor  `(,(plist-get lia/base16-colors :base0B) box)
+      evil-replace-state-cursor `(,(plist-get lia/base16-colors :base08) hbar)
+      evil-visual-state-cursor  `(,(plist-get lia/base16-colors :base09) box))
+
+;; set font
 (set-face-attribute 'default nil :font "Source Code Pro 10")
 
-(defun lia/tone-down-fringes ()
-  "Set the fringe colour to the background colour."
-  (set-face-attribute 'fringe nil
-                      :foreground (face-foreground 'default)
-                      :background (face-background 'default)))
-(lia/tone-down-fringes)
-
+;; highlight the current line
 (global-hl-line-mode 1)
 
-(setq-default mode-line-format
+;; custom mode line
+;; currently not in use. change to "setq-default" to use
+;; https://emacs-fu.blogspot.ca/2011/08/customizing-mode-line.html
+(setq mode-line-format
               (list
                ;; Any changes since last save?
                " %* "
@@ -175,17 +235,36 @@
                                    'help-echo buffer-file-coding-system))
                "] "))
 
+;; set the fringe color to the background color
+;; http://emacs.stackexchange.com/a/5343
+(defun lia/tone-down-fringes ()
+  "Set the fringe colour to the background colour."
+  (set-face-attribute 'fringe nil
+                      :foreground (face-foreground 'default)
+                      :background (face-background 'default)))
+(lia/tone-down-fringes)
+
+;; don't display the welcome screen
 (setq inhibit-splash-screen t)
 (setq inhibit-startup-message t)
 
+;; ########
+;; Org mode
+;; ########
+
+;; hide formating characters
 (setq org-hide-emphasis-markers t)
 
+;; add timestamps when DONE
 (setq org-log-done 'time)
 
+;; set agenda directory
 (setq org-agenda-files '("~/Dropbox/"))
 
+;; custom ellipsis
 (setq org-ellipsis " ⤵")
 
+;; org source code languages
 (setq org-src-fontify-natively t)
 (org-babel-do-load-languages
  'org-babel-load-languages '((css . t)
@@ -200,12 +279,19 @@
                              (ruby . t)
                              (sh . t)))
 
+;; custom todo keywords
 (setq org-todo-keywords
       '((sequence "TODO(t)" "IN-PROGRESS(i)" "ON HOLD(h)" "WAITING(w)" "|" "DONE(d)" "CANCELED(c)")
         (sequence "[ ](T)" "[-](I)" "[*](W)" "|" "[X](D)")))
 
+;; #####
+;; Other
+;; #####
+
+;; start emacs as a server
 (server-start)
 
+;; goto the last change
 (global-set-key [(control meta .)] 'goto-last-change)
 
 ;; Write backup files to own directory
@@ -216,6 +302,7 @@
 ;; Make backups of files, even when they're in version control
 (setq vc-make-backup-files t)
 
+;; scroll one line at a time
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
 (setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
 (setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
@@ -258,11 +345,11 @@
       (message "Found duplicated word.")
     (message "End of buffer")))
 
-(defun lia/window-rotate ()
-  "Rotate your windows"
+(defun lia/window-swap ()
+  "Swap your windows"
   (interactive)
   (cond ((not (> (count-windows)1))
-         (message "You can't rotate a single window!"))
+         (message "You can't swap a single window!"))
         (t
          (setq i 1)
          (setq numWindows (count-windows))
@@ -283,8 +370,12 @@
              (set-window-start w2 s1)
              (setq i (1+ i)))))))
 
+;; enable wordwrap
 (global-visual-line-mode t)
 
+;; start evil mode
 (require 'evil-org 'evil)
 (evil-mode t)
+
+;; open todo list
 (find-file "~/Dropbox/todo.org")
