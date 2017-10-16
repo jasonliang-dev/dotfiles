b9db44a83b6c96a2bd15f41803727077b499a3b6
SummerJacket <dxmcxd@gmail.com>
2017-10-15 18:54:12 -0700

    openbox: change my rice a bit

--- scripts/scripts/startup.sh
+++ scripts/scripts/startup.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/env sh
 
-if pgrep i3 || pgrep bspwm > /dev/null; then
-    ~/scripts/polybar &
-elif pgrep openbox > /dev/null; then
-    tint2 &
-else
-    exit 1
-fi
-
 compton &
 dunst &
 ~/scripts/feh.sh &
+~/scripts/polybar.sh &
 setxkbmap -option caps:escape &
 xset m 0 0 &
 xset r rate 270 27 &
