f1974bca6268c70af6a04a910849dedafa0a6073
SummerJacket <dxmcxd@gmail.com>
2017-10-22 22:06:18 -0700

    openbox: tint2 moved, update feh script and openbox menu

--- scripts/scripts/feh.sh
+++ scripts/scripts/feh.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env sh
 
-feh --randomize --bg-fill ~/Dropbox/wallpapers/*
+wp_dir="$HOME/Dropbox/wallpapers/"
+
+case "$1" in
+	"anime") wp_dir="${wp_dir}anime/" ;;
+	"comfy") wp_dir="${wp_dir}comfy/" ;;
+	"flat") wp_dir="${wp_dir}flat/" ;;
+	"mnt") wp_dir="${wp_dir}mnt/" ;;
+	"moe") wp_dir="${wp_dir}moe/" ;;
+	"simple") wp_dir="${wp_dir}simple/" ;;
+esac
+
+feh --randomize --bg-fill ${wp_dir}*
