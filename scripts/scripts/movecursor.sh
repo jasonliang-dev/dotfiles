6a2ff8e3921daf065695c8044978c2487d213e5f
summerjacket <dxmcxd@gmail.com>
2018-04-26 14:33:34 -0700

    remove unused scripts

--- scripts/scripts/movecursor.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-xinput set-prop 12 "libinput Disable While Typing Enabled" 0
--- scripts/scripts/nodejs-install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get install -y nodejs
