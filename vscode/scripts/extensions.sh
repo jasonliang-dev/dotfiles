3e6433c3ce7de496fecf1c45ede2e9aa4afa5e31
summerjacket <dxmcxd@gmail.com>
2018-04-25 21:24:44 -0700

    switch to vscode

---
+++ vscode/scripts/extensions.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# Vim
+code --install-extention vscodevim.vim
+
+# Appearance
+code --install-extention akamud.vscode-theme-onelight
+code --install-extention zhuangtongfa.material-theme
+code --install-extention robertohuertasm.vscode-icons
+
+# Java
+code --install-extention vscjava.vscode-java-pack
+
+# JavaScript
+code --install-extention xabikos.javascriptsnippets
+code --install-extention dbaeumer.vscode-eslint
