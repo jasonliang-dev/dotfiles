fca158ea9859279baebfc831da00dab5b83e37c0
summerjacket <dxmcxd@gmail.com>
2018-09-30 09:03:18 -0700

    update

--- README.md
+++
@@ -1,5 +0,0 @@
-# Dotfiles
-
-My collection of config files. Although it seems more of an accumulation rather than a collection.
-
-~~I will be cleaning up this place soon.~~ I lied.
--- scripts/scripts/rofi.sh
+++ scripts/scripts/rofi.sh
@@ -2,13 +2,12 @@
 rofi \
     -show run \
     -fuzzy \
-    -lines 12 \
+    -lines 5 \
     -eh 1 \
     -width 100 \
-    -padding 300 \
-    -opacity 90 \
+    -padding 800 \
     -bw 0 \
-#    -color-enabled \
-#    -color-window "#E6263238" \
-#    -color-normal "#00000000","#B2CCD6","#00000000","#00000000","#EEFFFF" \
-    -font "source sans pro 24"
+    -color-enabled \
+    -color-window "#BB263238" \
+    -color-normal "#00000000","#B2CCD6","#00000000","#00000000","#EEFFFF" \
+    -font "Roboto Regular 24"
