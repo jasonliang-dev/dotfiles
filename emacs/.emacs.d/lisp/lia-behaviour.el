5d808a772654111b579d6d32cef0ed868154fca5
summerjacket <dxmcxd@gmail.com>
2018-11-04 20:47:33 -0800

    reignite my love for emacs

---
+++ emacs/.emacs.d/lisp/lia-behaviour.el
@@ -0,0 +1,17 @@
+;;; lia-behaviour.el --- Emacs Config
+
+;;; Commentary:
+
+;;
+;; Change how Emacs behaves (+ Keybindings).
+;;
+
+;;; Code:
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(setq inhibit-startup-screen t)
+
+(provide 'lia-behaviour)
+
+;;; lia-behaviour.el ends here
