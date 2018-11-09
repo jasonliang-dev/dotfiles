775b7c20ea6860065a811fabffad5f7dbaeaeb95
summerjacket <dxmcxd@gmail.com>
2018-11-08 19:17:13 -0800

    I give up I'm using customize.
    set face attribute ain't working. I spent way too much time on
    this. The only thing I want is to make emacs look pretty. Is that too
    much to ask?

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -54,6 +54,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(outline-1 ((t (:background nil :height 1.0)))))
 
 ;;; init.el ends here
