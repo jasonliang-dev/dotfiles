7300ea4f5588595768517c56f751cc12c978cb63
summerjacket <dxmcxd@gmail.com>
2018-11-12 10:59:59 -0800

    remove unused

--- scripts/scripts/startup.sh
+++ scripts/scripts/startup.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env sh
 
+setxkbmap -option caps:escape
+xset m 0 0
+xset r rate 270 27
+xinput set-prop 12 "libinput Disable While Typing Enabled" 0
+~/scripts/feh.sh tile
+
 # compton &
 dunst &
-~/scripts/feh.sh tile &
 tint2 &
-~/scripts/polybar.sh &
-setxkbmap -option caps:escape &
-xset m 0 0 &
-xset r rate 270 27 &
-xinput set-prop 12 "libinput Disable While Typing Enabled" 0
+# ~/scripts/polybar.sh &
