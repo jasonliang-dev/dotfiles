40a1e9051ffee6a6b5ebdc03cf55fb74b1517e82
summerjacket <dxmcxd@gmail.com>
2018-12-16 20:28:31 -0800

    change mouse scroll speed

--- emacs/.emacs.d/lisp/lia-behaviour.el
+++ emacs/.emacs.d/lisp/lia-behaviour.el
@@ -141,7 +141,7 @@
 (setq auto-save-default nil)
 
 ;; scroll one line at a time (less "jumpy" than defaults)
-(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)) ;; one line at a time
+(setq mouse-wheel-scroll-amount '(2 ((shift) . 1)) ;; one line at a time
       mouse-wheel-progressive-speed nil ;; don't accelerate scrolling
       mouse-wheel-follow-mouse 't ;; scroll window under mouse
       scroll-step 1) ;; keyboard scroll one line at a time
