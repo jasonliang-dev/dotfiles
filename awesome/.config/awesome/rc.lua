7feeecd79ea75afd982511a7b443d6578131477a
summerjacket <dxmcxd@gmail.com>
2019-04-13 00:08:55 -0700

    fix maximized windows being, well... not maximized...

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -450,7 +450,8 @@ awful.rules.rules = {
                      keys = clientkeys,
                      buttons = clientbuttons,
                      screen = awful.screen.preferred,
-                     placement = awful.placement.no_overlap+awful.placement.no_offscreen
+                     placement = awful.placement.no_overlap+awful.placement.no_offscreen,
+                     size_hints_honor = false
      }
     },
 
