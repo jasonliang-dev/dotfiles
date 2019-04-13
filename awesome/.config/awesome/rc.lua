38fe4e6fe0179010d5f356bf331f811928a6e1bb
summerjacket <dxmcxd@gmail.com>
2019-04-13 00:08:37 -0700

    remove keyboard layout indicator

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -131,9 +131,6 @@ mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
 menubar.utils.terminal = terminal -- Set the terminal for applications that require it
 -- }}}
 
--- Keyboard map indicator and switcher
-mykeyboardlayout = awful.widget.keyboardlayout()
-
 -- {{{ Wibar
 -- Create a textclock widget
 mytextclock = wibox.widget.textclock()
@@ -234,7 +231,6 @@ awful.screen.connect_for_each_screen(function(s)
         s.mytasklist, -- Middle widget
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
-            mykeyboardlayout,
             wibox.widget.systray(),
             mytextclock,
             s.mylayoutbox,
