cc46f40a54c5d60d4785cbd4c48eea9f9c57f41b
summerjacket <dxmcxd@gmail.com>
2019-04-13 22:42:04 -0700

    change rofi appearance

--- scripts/scripts/rofi.sh
+++ scripts/scripts/rofi.sh
@@ -5,10 +5,10 @@ rofi \
     -fuzzy \
     -lines 5 \
     -eh 1 \
-    -width 50 \
+    -width 40 \
     -padding 80 \
     -bw 0 \
     -color-enabled \
     -color-window "#EE1D1F21","#00000000","#00000000" \
     -color-normal "#00000000","#FF969896","#00000000","#00000000","#FFC5C8C6" \
-    -font "Roboto Regular 18"
+    -font "Roboto Regular 20"
