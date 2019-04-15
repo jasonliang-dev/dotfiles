883669635eb7b974c915ae48ee67c5999e2e1f20
summerjacket <dxmcxd@gmail.com>
2019-04-14 23:03:19 -0700

    replace layout images

--- awesome/.config/awesome/themes/default/theme.lua
+++ awesome/.config/awesome/themes/default/theme.lua
@@ -131,15 +131,16 @@ theme.titlebar_maximized_button_focus_active  = themes_path .. "/titlebar/maximi
 theme.wallpaper = themes_path .. "/background.png"
 
 -- You can use your own layout icons like this:
+theme.layout_floating  = themes_path .. "/layouts/floatingw.png"
+theme.layout_tile = themes_path .. "/layouts/tilew.png"
+theme.layout_max = themes_path .. "/layouts/maxw.png"
+--[[
 theme.layout_fairh = themes_path .. "/layouts/fairhw.png"
 theme.layout_fairv = themes_path .. "/layouts/fairvw.png"
-theme.layout_floating  = themes_path .. "/layouts/floatingw.png"
 theme.layout_magnifier = themes_path .. "/layouts/magnifierw.png"
-theme.layout_max = themes_path .. "/layouts/maxw.png"
 theme.layout_fullscreen = themes_path .. "/layouts/fullscreenw.png"
 theme.layout_tilebottom = themes_path .. "/layouts/tilebottomw.png"
 theme.layout_tileleft   = themes_path .. "/layouts/tileleftw.png"
-theme.layout_tile = themes_path .. "/layouts/tilew.png"
 theme.layout_tiletop = themes_path .. "/layouts/tiletopw.png"
 theme.layout_spiral  = themes_path .. "/layouts/spiralw.png"
 theme.layout_dwindle = themes_path .. "/layouts/dwindlew.png"
@@ -147,6 +148,7 @@ theme.layout_cornernw = themes_path .. "/layouts/cornernww.png"
 theme.layout_cornerne = themes_path .. "/layouts/cornernew.png"
 theme.layout_cornersw = themes_path .. "/layouts/cornersww.png"
 theme.layout_cornerse = themes_path .. "/layouts/cornersew.png"
+--]]
 
 -- Generate Awesome icon:
 theme.awesome_icon = theme_assets.awesome_icon(
