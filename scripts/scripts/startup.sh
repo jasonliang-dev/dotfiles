8d985abb1b49f361c775199fe17154e07e551ffa
summerjacket <dxmcxd@gmail.com>
2019-04-16 23:01:32 -0700

    startup script now accepts command line arguments

--- scripts/scripts/startup.sh
+++ scripts/scripts/startup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 setxkbmap -option caps:escape
 xset m 0 0
@@ -8,6 +8,19 @@ xinput set-prop 12 "libinput Disable While Typing Enabled" 0
 
 compton &
 nm-applet &
-# dunst &
-# tint2 &
-# ~/scripts/polybar.sh &
+
+while [ "$#" -gt 0 ]
+do
+    if [[ $1 = "--dunst" ]]
+    then
+        dunst &
+    elif [[ $1 == "--tint2" ]]
+    then
+        tint2 &
+    elif [[ $1 == "--polybar" ]]
+    then
+        ~/scripts/polybar.sh &
+    fi
+
+    shift
+done
