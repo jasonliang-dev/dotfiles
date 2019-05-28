ce408bb8b83ee925e5072dbe565e0974b5811dee
summerjacket <dxmcxd@gmail.com>
2019-05-28 16:36:46 -0700

    add evil-commentary

--- emacs/.emacs.d/lisp/lia-evil.el
+++ emacs/.emacs.d/lisp/lia-evil.el
@@ -22,12 +22,12 @@
   :general
   (:states
    '(normal visual motion)
-   [remap evil-next-line] 'evil-next-visual-line
-   [remap evil-previous-line] 'evil-previous-visual-line
+   [remap evil-next-line]         'evil-next-visual-line
+   [remap evil-previous-line]     'evil-previous-visual-line
    [remap evil-beginning-of-line] 'evil-beginning-of-visual-line
-   [remap evil-end-of-line] 'evil-end-of-visual-line
-   [remap evil-scroll-line-down] 'lia/evil-scroll-line-down
-   [remap evil-scroll-line-up] 'lia/evil-scroll-line-up)
+   [remap evil-end-of-line]       'evil-end-of-visual-line
+   [remap evil-scroll-line-down]  'lia/evil-scroll-line-down
+   [remap evil-scroll-line-up]    'lia/evil-scroll-line-up)
   :init
   (setq evil-want-C-u-scroll t
         evil-want-keybinding nil
@@ -35,17 +35,22 @@
 
 (use-package evil-collection
   :ensure t
-  :after (evil)
+  :after evil
   :config (evil-collection-init))
+
+(use-package evil-commentary
+  :ensure t
+  :after evil
+  :config (evil-commentary-mode))
 
 (use-package evil-surround
   :ensure t
-  :after (evil)
+  :after evil
   :config (global-evil-surround-mode))
 
 (use-package evil-magit
   :ensure t
-  :after (evil magit))
+  :after evil magit)
 
 (provide 'lia-evil)
 
