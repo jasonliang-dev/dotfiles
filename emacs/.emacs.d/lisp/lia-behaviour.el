0b445cc21bec540d2701c47a980e79c9bd618f90
Jason Liang <jasonliang512@gmail.com>
2019-08-15 00:41:33 -0700

    emacs: disable wakatime-mode

--- emacs/.emacs.d/lisp/lia-behaviour.el
+++ emacs/.emacs.d/lisp/lia-behaviour.el
@@ -132,7 +132,8 @@
   (add-to-list 'projectile-globally-ignored-directories "vendor"))
 
 (use-package wakatime-mode
-  :ensure t
+  :ensure nil
+  :disabled t
   :defer 5
   :config (global-wakatime-mode))
 
