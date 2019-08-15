281a376bca7f4003cc05c5545f229f963a430d55
Jason Liang <jasonliang512@gmail.com>
2019-08-15 12:55:41 -0700

    emacs: remove unused iedit functions

--- emacs/.emacs.d/lisp/lia-keybind.el
+++ emacs/.emacs.d/lisp/lia-keybind.el
@@ -51,18 +51,6 @@ otherwise, run `ansi-term' with user shell."
   (defvar org-directory)
   (find-file org-directory))
 
-(defun lia/iedit ()
-  "Execute `iedit-mode' and `evil-iedit-state'."
-  (interactive)
-  (iedit-mode)
-  (evil-iedit-state))
-
-(defun lia/iedit-insert ()
-  "Execute `iedit-mode' and `evil-iedit-insert-state'."
-  (interactive)
-  (iedit-mode)
-  (evil-iedit-insert-state))
-
 (use-package general
   :ensure t
   :config
