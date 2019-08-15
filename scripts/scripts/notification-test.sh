31960be6aa2e56e098c1899cc76863f7cef18426
Jason Liang <jasonliang512@gmail.com>
2019-08-15 16:46:41 -0700

    scripts: added notification test script

---
+++ scripts/scripts/notification-test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+notify-send -u low "Message from someone" "I'm at the supermarket. Want me to buy some mild salsa?"
+
+notify-send "Message from Kanye West" "You're the Kanye best"
+
+notify-send -u critical "Battery Low" "Your battery is at -NaN%. Please reconsider being a JavaScript developer"
+
