315518de51de226a762ab574ed9e1ef5d1a353e0
Jason Liang <jasonliang512@gmail.com>
2019-08-16 18:30:22 -0700

    wal: wal-set.sh handles tint2

--- scripts/scripts/wal-set.sh
+++ scripts/scripts/wal-set.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 cp ~/.cache/wal/colors-themerc ~/dotfiles/openbox/.themes/dark/openbox-3/themerc
+cp ~/.cache/wal/colors-tint2rc ~/dotfiles/openbox/.config/tint2/tint2rc
 
 openbox --reconfigure
+killall -SIGUSR1 tint2
