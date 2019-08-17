f0c14b6110ac993329733ee37e4260145e8f741d
Jason Liang <jasonliang512@gmail.com>
2019-08-16 19:18:29 -0700

    wal: wal-set handles dunst

--- scripts/scripts/wal-set.sh
+++ scripts/scripts/wal-set.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-cp ~/.cache/wal/colors-themerc ~/dotfiles/openbox/.themes/dark/openbox-3/themerc
-cp ~/.cache/wal/colors-tint2rc ~/dotfiles/openbox/.config/tint2/tint2rc
+cp ~/.cache/wal/colors-themerc ~/.themes/dark/openbox-3/themerc
+cp ~/.cache/wal/colors-tint2rc ~/.config/tint2/tint2rc
+cp ~/.cache/wal/colors-dunstrc ~/.config/dunst/dunstrc
 
 openbox --reconfigure
 killall -SIGUSR1 tint2
+killall dunst
+
+~/scripts/feh.sh
+dunst &
