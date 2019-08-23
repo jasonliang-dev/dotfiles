3c8d2849f3bf42728ac8abbfa7623721db88d89b
Jason Liang <jasonliang512@gmail.com>
2019-08-22 22:44:38 -0700

    scripts: add comments to startup script. handle bar toggle flag

--- scripts/scripts/startup.sh
+++ scripts/scripts/startup.sh
@@ -2,14 +2,21 @@
 
 . $(dirname "$0")/shared
 
+# set caps lock to escape
 setxkbmap -option caps:escape &
+# no mouse acceleration
 xset m 0 0 &
+# set keyboard repeat rate
 xset r rate 200 50 &
+# I want to move the mouse and hold down keys at the same time
 xinput set-prop 12 "libinput Disable While Typing Enabled" 0 &
+# set background
 ~/scripts/feh.sh &
-
+# use compton
 compton &
+# use network manager applet
 nm-applet &
+# lock screen after 10 minutes
 xautolock -time 10 -locker $HOME/scripts/lock.sh &
 
 while [ "$#" -gt 0 ]
@@ -18,13 +25,16 @@ do
         "--dunst")
             dunst &
             ;;
+        "--polybar")
+            ~/scripts/polybar.sh $BAR_NAME &
+
+            if "$BAR_TOGGLE_ENABLED"; then
+                echo 1 > $BAR_TOGGLE
+                ~/scripts/bar-idle.sh &
+            fi
+            ;;
         "--tint2")
             tint2 &
-            ;;
-        "--polybar")
-            echo 1 > $BAR_TOGGLE
-            ~/scripts/polybar.sh &
-            ~/scripts/bar-idle.sh &
             ;;
     esac
 
