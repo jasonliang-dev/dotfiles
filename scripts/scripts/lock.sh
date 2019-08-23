99b7645f7f3f596a240d6cfdfdf1c8c6b6650a57
Jason Liang <jasonliang512@gmail.com>
2019-08-22 22:45:28 -0700

    scripts: add betterlockscreen to lock script

--- scripts/scripts/lock.sh
+++ scripts/scripts/lock.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-slock
+~/scripts/try.sh "betterlockscreen -l dim" slock
--- scripts/scripts/wal-set.sh
+++ scripts/scripts/wal-set.sh
@@ -4,13 +4,23 @@
 
 echo 1 > $BAR_TOGGLE
 
+# copy configs
 cp ~/.cache/wal/colors-themerc ~/.themes/dark/openbox-3/themerc
 cp ~/.cache/wal/colors-tint2rc ~/.config/tint2/tint2rc
 cp ~/.cache/wal/colors-dunstrc ~/.config/dunst/dunstrc
 cp ~/.cache/wal/colors-polybar ~/.config/polybar/config
 
+# reconfigure openbox. shows new theme
 openbox --reconfigure
-killall dunst
 
+# run feh with new background
 ~/scripts/feh.sh
+
+# set lockscreen background
+betterlockscreen -u "$(< "${HOME}/.cache/wal/wal")"
+
+# restart dunst
+killall dunst
+# dunst starts automatically with `notify-send` but not with spotify
+# notifications. start dunst manually.
 dunst &
