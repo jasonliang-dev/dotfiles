e34c0e284a8882b60cc6d4dcd2c62b499b340add
Jason Liang <jasonliang512@gmail.com>
2019-08-26 14:00:37 -0700

    scripts: add redshift to startup

--- scripts/scripts/startup.sh
+++ scripts/scripts/startup.sh
@@ -10,15 +10,21 @@ xset m 0 0 &
 xset r rate 200 50 &
 # I want to move the mouse and hold down keys at the same time
 xinput set-prop 12 "libinput Disable While Typing Enabled" 0 &
+
 # set background
 ~/scripts/feh.sh &
 # use compton
 compton &
+
 # use network manager applet
 nm-applet &
+# I like my eyes and I like sleep
+redshift-gtk &
+
 # lock screen after 10 minutes
 xautolock -time 10 -locker $HOME/scripts/lock.sh &
 
+# optional stuff
 while [ "$#" -gt 0 ]
 do
     case $1 in
