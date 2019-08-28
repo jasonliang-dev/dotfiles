d42450ad2de32695ea47625c31b31ff53cf12a99
Jason Liang <jasonliang512@gmail.com>
2019-08-28 11:02:34 -0700

    xmonad: use ResizableTall layout. Added directional resize bindings

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -8,34 +8,35 @@ import           Control.Arrow                  ( Arrow
 import           Control.Exception              ( catch
                                                 , SomeException
                                                 )
-import           GHC.Generics                   ( Generic )
-import           System.Exit                    ( exitWith
-                                                , ExitCode(..)
-                                                )
-import           System.Directory               ( getHomeDirectory )
-import           Data.Aeson                     ( decode
-                                                , FromJSON
-                                                )
-import           Data.Text                      ( Text
-                                                , unpack
-                                                )
+import qualified Data.Aeson                    as Aeson
+import           Data.Aeson                     ( FromJSON )
+import qualified Data.Text                     as T
+import           Data.Text                      ( Text )
 import qualified Data.ByteString.Lazy          as BS
 import           Data.ByteString.Lazy           ( ByteString )
 import qualified Data.Map                      as M
 import           Data.Map                       ( Map )
+import           GHC.Generics                   ( Generic )
+import           System.Directory               ( getHomeDirectory )
+import           System.Exit                    ( exitWith
+                                                , ExitCode(..)
+                                                )
 
 -- xmonad core
 import           XMonad
 import qualified XMonad.StackSet               as W
 
 -- xmonad contrib
-import           XMonad.Util.SpawnOnce          ( spawnOnce )
-import           XMonad.Layout.NoBorders        ( noBorders
-                                                , smartBorders
+import           XMonad.Layout.ResizableTile    ( ResizableTall(..)
+                                                , MirrorResize(..)
                                                 )
 import           XMonad.Hooks.EwmhDesktops      ( ewmh
                                                 , fullscreenEventHook
                                                 )
+import           XMonad.Layout.NoBorders        ( noBorders
+                                                , smartBorders
+                                                )
+import           XMonad.Util.SpawnOnce          ( spawnOnce )
 
 -- | Colors type derived from pywal generated json
 --
@@ -79,7 +80,7 @@ instance FromJSON ColorPalette
 myLayout = tiled ||| Mirror tiled ||| noBorders Full
  where
   -- default tiling algorithm partitions the screen into two panes
-  tiled   = Tall nmaster delta ratio
+  tiled   = ResizableTall nmaster delta ratio []
   -- The default number of windows in the master pane
   nmaster = 1
   -- Default proportion of screen occupied by master pane
@@ -89,112 +90,119 @@ myLayout = tiled ||| Mirror tiled ||| noBorders Full
 
 -- | The xmonad key bindings. Add, modify or remove key bindings here.
 --
--- (The comment formatting character is used when generating the manpage)
+-- Media keys, application shortcuts, etc defined in sxhkd.
 --
 myKeys :: XConfig Layout -> Map (KeyMask, KeySym) (X ())
 myKeys conf@(XConfig { XMonad.modMask = modMask }) =
   M.fromList
     $
-  --------------------------------------------------------------------
-  -- "Standard" xmonad key bindings
-  --
+       -- Close focused window.
+       [ ( (modMask .|. shiftMask, xK_q)
+         , kill
+         )
 
-  -- Close focused window.
-       [ ((modMask .|. shiftMask, xK_q), kill)
+       -- Launch dmenu.
        , ( (modMask, xK_p)
          , spawn "dmenu_run"
          )
 
-  -- Cycle through the available layout algorithms.
+       -- Cycle through the available layout algorithms.
        , ( (modMask, xK_space)
          , sendMessage NextLayout
          )
 
-  --  Reset the layouts on the current workspace to default.
+       --  Reset the layouts on the current workspace to default.
        , ( (modMask .|. shiftMask, xK_space)
          , setLayout $ XMonad.layoutHook conf
          )
 
-  -- Resize viewed windows to the correct size.
+       -- Resize viewed windows to the correct size.
        , ( (modMask, xK_n)
          , refresh
          )
 
-  -- Move focus to the next window.
+       -- Move focus to the next window.
        , ( (modMask, xK_Tab)
          , windows W.focusDown
          )
 
-  -- Move focus to the next window.
+       -- Move focus to the next window.
        , ( (modMask, xK_j)
          , windows W.focusDown
          )
 
-  -- Move focus to the previous window.
+       -- Move focus to the previous window.
        , ( (modMask, xK_k)
          , windows W.focusUp
          )
 
-  -- Swap the focused window and the master window.
+       -- Swap the focused window and the master window.
        , ( (modMask, xK_m)
          , windows W.swapMaster
          )
 
-  -- Swap the focused window with the next window.
+       -- Swap the focused window with the next window.
        , ( (modMask .|. shiftMask, xK_j)
          , windows W.swapDown
          )
 
-  -- Swap the focused window with the previous window.
+       -- Swap the focused window with the previous window.
        , ( (modMask .|. shiftMask, xK_k)
          , windows W.swapUp
          )
 
-  -- Shrink the master area.
-       , ( (modMask, xK_h)
+       -- Shrink master horizontally. Resize left.
+       , ( (modMask .|. controlMask, xK_h)
          , sendMessage Shrink
          )
-
-  -- Expand the master area.
-       , ( (modMask, xK_l)
+       -- Expand master horizontally. Resize right.
+       , ( (modMask .|. controlMask, xK_l)
          , sendMessage Expand
          )
+       -- Shrink master vertically. Resize down.
+       , ( (modMask .|. controlMask, xK_j)
+         , sendMessage MirrorShrink
+         )
+       -- Expand master vertically. Resize up.
+       , ( (modMask .|. controlMask, xK_k)
+         , sendMessage MirrorExpand
+         )
 
-  -- Push window back into tiling.
+       -- Push window back into tiling.
        , ( (modMask, xK_t)
          , withFocused $ windows . W.sink
          )
 
-  -- Increment the number of windows in the master area.
+       -- Increment the number of windows in the master area.
        , ( (modMask, xK_comma)
          , sendMessage (IncMasterN 1)
          )
 
-  -- Decrement the number of windows in the master area.
+       -- Decrement the number of windows in the master area.
        , ( (modMask, xK_period)
          , sendMessage (IncMasterN (-1))
          )
 
-  -- Quit xmonad.
+       -- Quit xmonad.
        , ( (modMask .|. shiftMask .|. controlMask, xK_q)
          , io (exitWith ExitSuccess)
          )
 
-  -- Restart xmonad.
+       -- Restart xmonad.
        , ((modMask, xK_q), restart "xmonad" True)
        ]
     ++
 
-  -- mod-[1..9], Switch to workspace N
-  -- mod-shift-[1..9], Move client to workspace N
+       -- mod-[1..9], Switch to workspace N
+       -- mod-shift-[1..9], Move client to workspace N
        [ ((m .|. modMask, k), windows $ f i)
        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
        ]
     ++
 
-  -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
-  -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
+       -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
+       -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
        [ ( (m .|. modMask, key)
          , screenWorkspace sc >>= flip whenJust (windows . f)
          )
@@ -223,13 +231,13 @@ getColors :: IO (Text, Text)
 getColors = do
   home <- getHomeDirectory
   json <- safeReadFile $ home ++ "/.cache/wal/colors.json"
-  return $ case decode json :: Maybe Colors of
+  return $ case Aeson.decode json :: Maybe Colors of
     Nothing  -> ("#222222", "#808080")
-    Just col -> (background $ special col, foreground $ special col)
+    Just col -> (background $ special col, color6 $ colors col)
 
 main :: IO ()
 main = do
-  (normalColor, focusedColor) <- mapTuple unpack <$> getColors
+  (normalColor, focusedColor) <- mapTuple T.unpack <$> getColors
   xmonad $ ewmh def
     { terminal           = "~/scripts/term.sh"
     , modMask            = mod4Mask
