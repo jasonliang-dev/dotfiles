cba8126d1c6ac37af3faf7ddc70a2673be89c395
Jason Liang <jasonliang512@gmail.com>
2019-08-30 12:51:26 -0700

    scripts: volume info script shows if sound is muted

--- scripts/scripts/info/volume.sh
+++ scripts/scripts/info/volume.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env sh
 
-amixer sget Master | awk -F "[][]" '/%/ { print $2; exit }'
+AMIX_OUT=$(amixer get Master)
+
+if echo "$AMIX_OUT" | grep "\[off\]" > /dev/null 2>&1; then
+    echo Muted
+else
+    echo "$AMIX_OUT" | awk -F "[][]" '/%/ { print $2; exit }'
+fi
+
+
