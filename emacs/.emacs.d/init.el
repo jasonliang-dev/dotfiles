f97c0be3ee2914c0bab13bbb36f7d75e96e2807d
Jason Liang <jasonliang512@gmail.com>
2019-09-23 23:58:19 -0700

    emacs: finish adding documentation to config.org

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -4,29 +4,33 @@
 
 ;; Bootstrap `config.org' file.
 ;;
-;; Since I care about startup time, I'm not using
-;; `org-babel-load-file'.  Instead, I'm using some wonderful code
-;; found on a blog post by Malabarba:
+;; Typically, a literate Emacs config would be loaded like so:
+;;
+;; (org-babel-load-file "~/.emacs.d/config.org")
+;;
+;; But since I care about startup time, I'm not using
+;; `org-babel-load-file'.
+;;
+;; Instead, I'm loading my config using something I found on a blog
+;; post by Malabarba:
 ;; http://endlessparentheses.com/init-org-Without-org-mode.html
+;;
+;; I've edited their solution to suit my needs (no messaging, support
+;; for multiple level 1 headers).
 
 ;;; Code:
 
-(defvar lia/config.org-message-depth 3
-  "What depth of init.org headers to message at startup.")
-
 (with-temp-buffer
-  (insert-file-contents "~/.emacs.d/config.org")
-  (goto-char (point-min))
+  (insert-file-contents
+   (expand-file-name "config.org" user-emacs-directory))
+  (search-forward "#+BEGIN_SRC")
+  (move-beginning-of-line 1)
   (while (not (eobp))
-    (forward-line 1)
-    (cond
-     ;; Evaluate Code Blocks
-     ((looking-at "^#\\+BEGIN_SRC +emacs-lisp.*$")
-      (let ((l (match-end 0)))
-        (search-forward "\n#+END_SRC")
-        (eval-region l (match-beginning 0))))
-     ;; Finish on the next level-1 header
-     ((looking-at "^\\* ")
-      (goto-char (point-max))))))
+    (if (looking-at "^#\\+BEGIN_SRC +emacs-lisp.*$")
+        ;; Evaluate Code Blocks
+        (let ((l (match-end 0)))
+          (search-forward "\n#+END_SRC")
+          (eval-region l (match-beginning 0))))
+    (forward-line 1)))
 
 ;;; init.el ends here
