44b298372dbd6279fc3c3c70a11a31814a6d1c03
Jason Liang <jasonliang512@gmail.com>
2019-11-11 01:28:00 -0800

    scripts: add center-window.sh

---
+++ scripts/scripts/center-window.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# from: https://askubuntu.com/a/571711
+
+IFS='x' read screenWidth screenHeight < <(xdpyinfo | grep dimensions | grep -o '[0-9x]*' | head -n1)
+
+width=$(xdotool getactivewindow getwindowgeometry --shell | head -4 | tail -1 | sed 's/[^0-9]*//')
+height=$(xdotool getactivewindow getwindowgeometry --shell | head -5 | tail -1 | sed 's/[^0-9]*//')
+
+newPosX=$((screenWidth/2-width/2))
+newPosY=$((screenHeight/2-height/2))
+
+xdotool getactivewindow windowmove "$newPosX" "$newPosY"
