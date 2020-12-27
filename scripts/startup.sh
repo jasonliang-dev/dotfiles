8a659695ca323f6185d064ced07e815d126aa227
Jason Liang <jasonliang512@gmail.com>
2020-12-27 00:27:55 -0800

    startup script also starts up bar

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,20 +1,16 @@
 #!/usr/bin/env sh
 
-# caps -> escape
+# caps -> escape, right alt -> compose
 setxkbmap -option caps:escape
-
-# right alt -> compose
 setxkbmap -option compose:ralt
-
-# no mouse acceleration
-xset m 0 0
-
 # keyboard repeat rate
 xset r rate 180 45
 
+# no mouse acceleration
+xset m 0 0
 # move mouse cursor with trackpad and type keys at the same time
 xinput set-prop 12 "libinput Disable While Typing Enabled" 0
 
 feh --bg-fill --randomize ~/wallpapers/* &
-
 picom --config ~/.picom.conf &
+~/scripts/bar.sh &
